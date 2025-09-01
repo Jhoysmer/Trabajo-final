27cf75e8f863f837e53026fa5f06effd60d2c358
Agustin Palacio <aguspalacio248@gmail.com>
2025-09-01 17:43:17 -0300

    1-9

---
+++ TP_Final/jsj.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Menú de perfil
+  const profileBtn = document.querySelector(".profile-icon");
+  const dropdown = document.getElementById("dropdown-menu");
+
+  if (profileBtn) {
+    profileBtn.addEventListener("click", (e) => {
+      e.stopPropagation(); // Previene que se cierre inmediatamente
+      dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+    });
+
+    // Oculta el menú si se hace clic fuera
+    document.addEventListener("click", () => {
+      dropdown.style.display = "none";
+    });
+  }
+});
