fb75a6cbbf8bdd5a61fe0a1335824bd17930512d
Jacqueline <jacqueline2024csuiza@gmail.com>
2025-10-13 13:56:39 -0300

    ultimo cambio

---
+++ server/registro.php
@@ -0,0 +1,13 @@
+<?php
+$nom = $_POST["name"];
+$ape = $_POST["name"];
+$mail = $_POST["name"];
+$tel = $_POST["name"];
+$pais = $_POST["name"];
+
+
+echo $nom;
+
+
+echo "<h1>Hola </h1>";
+?>
--- sitie/registrarse.php
+++ sitie/registrarse.php
@@ -12,28 +12,27 @@
   <div class="register-container">
    
     <form id="registerForm">
-      <input type="text" id="name" placeholder="Nombre *" required>
-      <input type="text" id="username" placeholder="Nombre de Usuario *" required>
-      <input type="text" id="country" placeholder="Pais">
-      <input type="text" id="city" placeholder="Ciudad">
+      <input type="nom" id="name" placeholder="Nombre *" required>
+      <input type="un" id="username" placeholder="Nombre de Usuario *" required>
+      <input type="pai" id="country" placeholder="Pais">
+      <input type="cui" id="city" placeholder="Ciudad">
       <input type="tel" id="phone" placeholder="Telefono *" required>
-      <input type="email" id="email" placeholder="Email *" required>
-      <input type="password" id="password" placeholder="Contraseña *" required>
+       <input type="email"name="email" id="email" placeholder="Email *" required>
+      <input type="password"name="password" id="password" placeholder="Contraseña *" required>
       <input type="password" id="confirmPassword" placeholder="Confirmar contraseña *" required>
-      
+     
       <div class="terms">
         <input type="checkbox" id="terms">
         <label for="terms">Acepto términos y condiciones</label>
       </div>
-      
+     
       <button type="submit">REGISTRARSE</button>
     </form>
   </div>
 
-  <script>
-    document.getElementById("registerForm").addEventListener("submit", function(e){
-      e.preventDefault();
 
+  <script>
+  
       const name = document.getElementById("name").value;
       const username = document.getElementById("username").value;
       const country = document.getElementById("country").value;
@@ -44,12 +43,14 @@
       const confirmPassword = document.getElementById("confirmPassword").value;
       const terms = document.getElementById("terms").checked;
 
+
       if(password !== confirmPassword){
-        alert("❌ Las contraseñas no coinciden");
+        alert(" Las contraseñas no coinciden");
         return;
       }
 
-      alert(`✅ Registro exitoso
+
+      alert(` Registro exitoso
       \nNombre: ${name}
       \nUsuario: ${username}
       \nPaís: ${country}
@@ -57,10 +58,11 @@
       \nTeléfono: ${phone}
       \nEmail: ${email}
       \nAceptó términos: ${terms ? "Sí" : "No"}`);
-    });
+    
     /*Cuando se registre correctamente hay que hacer que el boton
      con un "if" redireccione al usuario al HTML: tp_F2*/
   </script>
+
 
 </body>
 </html>
